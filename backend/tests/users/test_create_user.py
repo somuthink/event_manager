4a8b73a4f9200600a36014d0aec5d253c9d9cc53
polukhow <misapoluhov350@gmail.com>
2025-05-02 20:49:11 +0500

    update

--- backend/tests/users/test_create_user.py
+++ backend/tests/users/test_create_user.py
@@ -38,3 +38,34 @@ def test_create_user():
   }
     response = client.delete(f"/users/{id}", headers=auth("super_user", "1"))
     assert response.status_code == 200
+
+
+def test_create_existed_user():
+    id = client.post("/users", json = {
+  "username": "some_username",
+  "email": "some_email",
+  "number": 666,
+  "name": "some_name",
+  "surname": "some_surname",
+  "patronymic": "some_patronymic",
+  "is_active": True,
+  "birthday": "2025-05-02",
+  "password": "some_password"
+    }).json().get("id")
+    response = client.post("/users", json = {
+  "username": "some_username",
+  "email": "some_email",
+  "number": 666,
+  "name": "some_name",
+  "surname": "some_surname",
+  "patronymic": "some_patronymic",
+  "is_active": True,
+  "birthday": "2025-05-02",
+  "password": "some_password"
+    })
+    assert response.status_code == 404
+    assert response.json() == {
+  "detail": "Username already exists"
+}
+    response = client.delete(f"/users/{id}", headers=auth("super_user", "1"))
+    assert response.status_code == 200
