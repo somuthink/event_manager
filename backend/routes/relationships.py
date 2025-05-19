f7def0e69a7093df2010f0a39adbb7d48a59e551
polukhow <misapoluhov350@gmail.com>
2025-05-19 17:45:44 +0500

    patch

--- backend/routes/relationships.py
+++ backend/routes/relationships.py
@@ -13,33 +13,21 @@ router = APIRouter(prefix="/relationships")
 @router.get("/user-event/event/{event_id}", response_model=list[schemas.Association_user])
 @check([Right(Access.UPDATE, Model.EVENT), Right(Access.READ, Model.USER)])
 def read_members_of_event(user: UserDep, event_id: int, skip: int = 0, limit: int = 0,  db: Session = Depends(get_db)):
-
-    if skip > 1:
-        return []
-
     db_event = crud.get_event(db, event_id)
-
     if db_event is None:
         raise HTTPException(status_code=404, detail="Event Not Found")
     else:
-        return db_event.members
+        return db_event.members[skip : skip + limit]
 
 
 @router.get("/user-event/user/{user_id}", response_model=list[schemas.Association_event])
 @owner_or_check([Right(Access.UPDATE, Model.EVENT), Right(Access.READ, Model.USER)])
-def read_user_events(user: UserDep, user_id: int,  skip: int = 0, limit: int = 0, db: Session = Depends(get_db)):
-
-    if skip > 1:
-        return []
-
+def read_user_events(user: UserDep, user_id: int, skip: int = 0, limit: int = 0, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id)
-
     if db_user is None:
         raise HTTPException(status_code=404, detail="User Not Found")
-    elif  limit == 1:
-        return [db_user.events[0]]
     else:
-        return db_user.events
+        return db_user.events[skip : skip + limit]
 
 
 @router.post("/user-event", response_model=schemas.Association)
@@ -69,28 +57,21 @@ def delete_user_event(user: UserDep, user_id: int, event_id: int, db: Session = 
 
 
 @router.get("/event-news/event/{event_id}", response_model=list[schemas.NewsRead])
-def read_event_news(user: UserDep, event_id: int,  skip: int = 0, limit: int = 0, db: Session = Depends(get_db)):
-
-    if skip > 1:
-        return []
-
+def read_event_news(user: UserDep, event_id: int, skip: int = 0, limit: int = 0, db: Session = Depends(get_db)):
     db_event = crud.get_event(db, event_id)
-
     if db_event is None:
         raise HTTPException(status_code=404, detail="Event Not Found")
-    elif limit == 1:
-        return [db_event.news[0]]
     else:
-        return db_event.news
+        return db_event.news[skip : skip + limit]
 
 
 @router.get("/event-news/news/{news_id}", response_model=list[schemas.EventRead])
-def read_news_events(user: UserDep, news_id: int, db: Session = Depends(get_db)):
-    db_news = crud.get_news(db, news_id )
+def read_news_events(user: UserDep, news_id: int, skip: int = 0, limit: int = 0, db: Session = Depends(get_db)):
+    db_news = crud.get_news(db, news_id)
     if db_news is None:
         raise HTTPException(status_code=404, detail="News Not Found")
     else:
-        return db_news.events
+        return db_news.events[skip : skip + limit]
 
 
 @router.post("/event-news")
